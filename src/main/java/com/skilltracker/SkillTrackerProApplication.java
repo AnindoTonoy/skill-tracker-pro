c00f52c9f0d5c9d42ac566c68ae2d75c3df5e5f2
anindo.dey <anindo.dey@bracits.com>
2025-09-15 23:47:56 +0600

    feat: setup Spring Boot project

---
+++ src/main/java/com/skilltracker/SkillTrackerProApplication.java
@@ -0,0 +1,13 @@
+package com.skilltracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkillTrackerProApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkillTrackerProApplication.class, args);
+	}
+
+}
